9bfee7768f5c8099a943e84caf1d853eedc1c319
kirill <kirill592007@gmail.com>
2026-09-05 13:19:50 +0600

    add new sponsor

--- src/utils/keyboards.js
+++ src/utils/keyboards.js
@@ -3,9 +3,10 @@ function startKeyboard() {
         inline_keyboard: [
             [{text: '➕ Подписаться', url: "https://t.me/+cqkrYv1GR4dlNGVi"}],
             [{text: '➕ Подписаться', url: "https://t.me/+JgxLhEaZGBswNWM1"}],
-            [{text: '➕ Оставить заявку', url: "https://t.me/+XuVy34031lc3ZjYy"}],
+            [{text: '➕ Оставить заявку', url: "https://t.me/+b9ue6Zd4AYZjZmUy"}],
             [{text: '➕ Нажать старт', url: "https://t.me/music_ros_bot?start=uglyscimxs2008"}],
             [{text: '➕ Нажать старт', url: "https://t.me/onlykrykhi_bot?start=adminref_TYnRJWm5s4Uk"}],
+            [{text: '➕ Нажать старт', url: "https://t.me/spineraapp_bot?start=src_5AP3AFEF"}],
             [{text: '✅Проверить', callback_data: "check"}],
             [{text: '👨‍🔧Tех. поддержка', callback_data: 'support'}],
             [{text: '🤝‍Сотрудничество (реклама)', callback_data: 'support_ad'}]
@@ -25,7 +26,6 @@ function doKeyboard() {
     return {
         inline_keyboard: [
             [{text: 'Поиск по коду🔎', callback_data: 'search'}],
-            // [{text: 'ИИ эксперт🤖', callback_data: 'ai'}],
             [{text: 'Рандомное аниме🎲', callback_data: 'search_random_anime'}],
             [{text: 'Рандомный фильм🎲', callback_data: 'search_random_film'}],
             [{text: 'Поиск по жанру🔎', callback_data: 'search_genre'}],
