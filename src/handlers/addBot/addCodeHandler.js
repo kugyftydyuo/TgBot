858276fcd8907f9d9c0273be5f50ce30157daf12
kirill <kirill592007@gmail.com>
2026-09-15 14:21:17 +0600

    fix bug

--- src/handlers/addBot/addCodeHandler.js
+++ src/handlers/addBot/addCodeHandler.js
@@ -7,6 +7,11 @@ export async function addCodeHandler(msg, bot) {
 
     if (admins.includes(userId)) {
         const session = getSession(userId);
+        session.data = {
+            code: null,
+            genre: "",
+            type: ""
+        }
         session.state = 'ADD_MOVIE_CODE';
         await bot.sendMessage(chatId, '📩 Добавление новой записи...\n\n✍ Напиши код под которым нужно добавить фильм');
     } else {
